844391b406e1748826390652c23cd8d780075c6e
kimyougyeong04 <133843607+kimyougyeong04@users.noreply.github.com>
2023-12-06 16:43:16 +0900

    Update_file_end

--- project_daum_movie/main.py
+++ project_daum_movie/main.py
@@ -1,11 +1,12 @@
-# TODO: 1.스케줄링
-#       2.리뷰 중복 체크(중복 수집 X)
+# TODO:
+#
 #       3.DB에 저장 된 데이터 Excel 다운로드
 #       4.DB에 저장 된 데이터 → 텍스트 분석
 #       5.DB에 저장 된 데이터 → WordCloud 시각화
 
-from collect.collect_daum_movie_review import  review_collector
-from db.movie_dao import get_last_review
+from collect.collect_daum_movie_review import review_collector
+from apscheduler.schedulers.blocking import BlockingScheduler
+
 
 def main():
     print("=" * 100)
@@ -24,14 +25,16 @@ def main():
     #   수집리뷰_date 비교 last_date
     #   ex) last_date = 2023.11.27 02:25     202311270225
     #   ex) 수집리뷰_date = 2023.11.28 02:25  202311280225
-    last_date = get_last_review()
 
-    if last_date == None:
-        last_date = 0
-    else:
-        last_date = int(last_date["int_regdate"])
 
-    review_collector(movie_code, last_date)
+
+    scheduler = BlockingScheduler()
+    scheduler.add_job(review_collector,
+                      args=[movie_code],
+                      trigger="cron",
+                      hour="12",
+                      minute="0")
+    scheduler.start()
 
 
 if __name__ == "__main__":
